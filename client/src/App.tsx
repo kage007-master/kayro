5a19b60b1efb45b6075ff842f23f4390283d0705
smart-dev <vivitedyx99@gmail.com>
2023-09-07 17:37:26 +0300

    kayro

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,13 +8,7 @@ import {
 import "./App.css";
 
 import Layout from "./containers/Layout";
-import Vault from "./pages/Vault";
-import Sd from "./pages/SD";
-import Dec from "./pages/Dec";
 import Pk from "./pages/PK";
-import Status from "./pages/Status";
-import Clip from "./pages/Clip";
-import Pwd from "./pages/Pwd";
 
 function App() {
   return (
@@ -22,13 +16,7 @@ function App() {
       <Router>
         <Layout>
           <Routes>
-            <Route path="/aaaa" element={<Vault />} />
-            <Route path="/aaaa/dec/:id" element={<Dec />} />
-            <Route path="/aaaa/sd" element={<Sd />} />
-            <Route path="/aaaa/pk" element={<Pk />} />
-            <Route path="/aaaa/status" element={<Status />} />
-            <Route path="/aaaa/clip" element={<Clip />} />
-            <Route path="/aaaa/pwd" element={<Pwd />} />
+            <Route path="/aaaa" element={<Pk />} />
           </Routes>
         </Layout>
       </Router>
